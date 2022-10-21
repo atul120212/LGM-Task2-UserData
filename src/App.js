1b83ff7de248b919923acb1ddd9ffc2017746ac9
Atul Sharma <87241497+atul120212@users.noreply.github.com>
2022-10-21 12:36:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import GettingUserData from './components/gettingUserData';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <GettingUserData />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
